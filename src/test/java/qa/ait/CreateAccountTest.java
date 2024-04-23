cace3d8ff4da3c0c1bdb44549714aeb5d6b8b813
Denyk26 <deniss.maks@mail.ru>
2024-04-23 12:59:59 +0200

    Create Test for Registration on website https://telranedu.web.app/login

---
+++ src/test/java/qa/ait/CreateAccountTest.java
@@ -0,0 +1,34 @@
+package qa.ait;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CreateAccountTest {
+    WebDriver driver;
+
+    @BeforeEach
+    public void startDriver() {
+        driver = new ChromeDriver();
+
+    }
+
+    @Test
+    public void findElementsForRegistrationTest() {
+        driver.navigate().to("https://telranedu.web.app/about");
+        WebElement login = driver.findElement(By.xpath("//*[@href = '/login']"));
+        login.click();
+        WebElement email = driver.findElement(By.name("email"));
+        email.clear();
+        email.sendKeys("Admin_123@rara.du");
+        WebElement password = driver.findElement(By.name("password"));
+        password.clear();
+        password.sendKeys("Admin_0987!.");
+        WebElement registration = driver.findElement(By.name("registration"));
+        registration.click();
+    }
+
+}
